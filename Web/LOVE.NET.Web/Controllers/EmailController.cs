d7276aeabf57d7e19adeb2c305448b776710d643
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-08-28 14:22:45 +0300

    Exposing email service through controller

---
+++ Web/LOVE.NET.Web/Controllers/EmailController.cs
@@ -0,0 +1,55 @@
+﻿namespace LOVE.NET.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using LOVE.NET.Services.Email;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static LOVE.NET.Common.GlobalConstants.ControllerRoutesConstants;
+    using static LOVE.NET.Common.GlobalConstants.EmailMessagesConstants;
+
+    [Route(EmailControllerName)]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route(VerifyEmailRoute)]
+        public async Task<IActionResult> VerifyEmail(string email, string token)
+        {
+            var result = await this.emailService.VerifyEmailAsync(email, token);
+
+            if (result.Failure)
+            {
+                return this.BadRequest(result.Error);
+            }
+
+            return this.Ok(EmailConfirmed);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route(ResendEmailConfirmationLinkRoute)]
+        public async Task<IActionResult> ResendEmailConfirmationLink(string email)
+        {
+            var origin = this.Request.Headers[HeaderOrigin];
+
+            var result = await this.emailService.ResendEmailConfirmationLinkAsync(email, origin);
+
+            if (result.Failure)
+            {
+                return this.BadRequest(result.Error);
+            }
+
+            return this.Ok();
+        }
+    }
+}
