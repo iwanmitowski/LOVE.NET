506330deb5b82c4bab15d11816a984c6b62449c4
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-10-07 21:35:42 +0300

    Having 2 axio instances preventing infinite loop. Refreshing token on 401 status code

--- Web/LOVE.NET.Web/ClientApp/src/services/api.js
+++ Web/LOVE.NET.Web/ClientApp/src/services/api.js
@@ -1,14 +1,61 @@
 import axios from "axios";
 import { globalConstants } from "../utils/constants";
 
-export default axios.create({
-  baseURL: globalConstants.API_URL
+export const instance = axios.create({
+  baseURL: globalConstants.API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const axiosInternal = axios.create({
-    baseURL: globalConstants.API_URL,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    withCredentials: true,
+  baseURL: globalConstants.API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const auth = localStorage.getItem("auth");
+
+    if (auth) {
+      const { token } = JSON.parse(auth);
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInternal.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    const originalConfig = err.config;
+
+    if (!originalConfig.url.includes("/identity/login") && err.response) {
+      if (err.response.status === 401 && !originalConfig._retry) {
+        originalConfig._retry = true;
+
+        try {
+          const response = await axiosInternal.post("/identity/refreshToken");
+
+          const { token } = response.data;
+          localStorage.setItem("auth", JSON.stringify(token));
+
+          originalConfig.headers["Authorization"] = `Bearer ${token}`;
+
+          return axiosInternal(originalConfig);
+        } catch (error) {
+          return Promise.reject(error);
+        }
+      }
+    }
+  }
+);
