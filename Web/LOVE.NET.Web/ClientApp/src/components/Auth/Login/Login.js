b2c9af3aff0d4bd437ef14b6755f32b8f4528bc8
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-10-09 13:36:53 +0300

    Styling login and register form

--- Web/LOVE.NET.Web/ClientApp/src/components/Auth/Login/Login.js
+++ Web/LOVE.NET.Web/ClientApp/src/components/Auth/Login/Login.js
@@ -5,10 +5,10 @@ import { Link, useNavigate } from "react-router-dom";
 import { useIdentityContext } from "../../../hooks/useIdentityContext";
 
 import * as identityService from "../../../services/identityService";
-
+import styles from "../Auth.module.css";
 
 export default function Login() {
-	const { userLogin	} = useIdentityContext();
+  const { userLogin } = useIdentityContext();
   const navigate = useNavigate();
 
   const [user, setUser] = useState({
@@ -35,9 +35,10 @@ export default function Login() {
   const onFormSubmit = (e) => {
     e.preventDefault();
 
-		identityService.login(user)
+    identityService
+      .login(user)
       .then((res) => {
-				userLogin(res);
+        userLogin(res);
         navigate("/");
       })
       .catch((error) => {
@@ -45,44 +46,48 @@ export default function Login() {
       });
   };
 
+  const formWrapperStyles = `${styles["form-wrapper"]} d-flex justify-content-center align-items-center`;
+
   return (
-    <div className="login-form-wrapper">
-      <Form onSubmit={onFormSubmit}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            defaultValue={user.email}
-            placeholder="Enter address"
-            onChange={onInputChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            defaultValue={user.password}
-            placeholder="Enter password"
-            onChange={onInputChange}
-            required
-          />
-        </Form.Group>
-				{error && <span className="text-danger">{error}</span>}
-        <Form.Group>
-          <p>
-            You don't have account ?
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-          </p>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+    <div className={formWrapperStyles}>
+      <div className={styles["input-fields-length"]}>
+        <Form onSubmit={onFormSubmit}>
+          <Form.Group className="form-group mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              defaultValue={user.email}
+              placeholder="Enter address"
+              onChange={onInputChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="form-group mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              defaultValue={user.password}
+              placeholder="Enter password"
+              onChange={onInputChange}
+              required
+            />
+          </Form.Group>
+          {error && <span className="text-danger">{error}</span>}
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+          <Form.Group>
+            <p>
+              You don't have account ?
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+            </p>
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 }
--- Web/LOVE.NET.Web/ClientApp/src/components/Auth/Register/Register.js
+++ Web/LOVE.NET.Web/ClientApp/src/components/Auth/Register/Register.js
@@ -1,10 +1,11 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useState, useEffect, Fragment } from "react";
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useIdentityContext } from "../../../hooks/useIdentityContext";
 
 import * as identityService from "../../../services/identityService";
+import styles from "../Auth.module.css";
 
 export default function Register() {
   const { userLogin } = useIdentityContext();
@@ -54,111 +55,115 @@ export default function Register() {
       });
   };
 
+  const formWrapperStyles = `${styles["form-wrapper"]} d-flex justify-content-center align-items-center`;
+
   return (
-    <div className="register-form-wrapper">
-      <Form onSubmit={onFormSubmit}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            name="email"
-            defaultValue={user.email}
-            placeholder="Enter address"
-            onChange={onInputChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="userName">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            name="userName"
-            defaultValue={user.userName}
-            placeholder="Enter username"
-            onChange={onInputChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            defaultValue={user.password}
-            placeholder="Enter password"
-            onChange={onInputChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="confirmPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control
-            type="password"
-            name="confirmPassword"
-            defaultValue={user.confirmPassword}
-            placeholder="Confirm password"
-            onChange={onInputChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="countryId">
-          <Form.Label>Choose country</Form.Label>
-          <Form.Select
-            className="mb-3"
-            name="countryId"
-            onChange={onInputChange}
-          >
-            <option value="0">Chooce country here</option>
-            <option value="1">first</option>
-            <option value="2">second</option>
-            <option value="3">third</option>
-          </Form.Select>
-        </Form.Group>
-        {!!parseInt(user.countryId) && (
-          <Form.Group className="mb-3" controlId="cityId">
-            {" "}
-            <Form.Label>Choose city</Form.Label>
+    <div className={formWrapperStyles}>
+      <div className={styles['input-fields-length']}>
+        <Form onSubmit={onFormSubmit}>
+          <Form.Group className="form-group mb-3" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              defaultValue={user.email}
+              placeholder="Enter address"
+              onChange={onInputChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="form-group mb-3" controlId="userName">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              name="userName"
+              defaultValue={user.userName}
+              placeholder="Enter username"
+              onChange={onInputChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="form-group mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              defaultValue={user.password}
+              placeholder="Enter password"
+              onChange={onInputChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="form-group mb-3" controlId="confirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="confirmPassword"
+              defaultValue={user.confirmPassword}
+              placeholder="Confirm password"
+              onChange={onInputChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="form-group mb-3" controlId="countryId">
+            <Form.Label>Choose country</Form.Label>
             <Form.Select
               className="mb-3"
-              name="cityId"
+              name="countryId"
               onChange={onInputChange}
             >
-              <option value="0">Choose city here</option>
+              <option value="0">Chooce country here</option>
               <option value="1">first</option>
               <option value="2">second</option>
               <option value="3">third</option>
             </Form.Select>
           </Form.Group>
-        )}
-        <Form.Group className="mb-3" controlId="information">
-          <Form.Label>Bio</Form.Label>
-          <Form.Control
-            as="textarea"
-            name="bio"
-            rows={5}
-            defaultValue={user.bio}
-            placeholder="Enter your bio"
-            onChange={onInputChange}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="profilePicture">
-          <Form.Label>Upload your photo</Form.Label>
-          <Form.Control type="file" />
-        </Form.Group>
-        {error && <span className="text-danger">{error}</span>}
-        <Button variant="primary" type="submit">
-          Register
-        </Button>
-        <Form.Group>
-          <p>
-            Already have account ?
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </p>
-        </Form.Group>
-      </Form>
+          {!!parseInt(user.countryId) && (
+            <Form.Group className="form-group mb-3" controlId="cityId">
+              {" "}
+              <Form.Label>Choose city</Form.Label>
+              <Form.Select
+                className="mb-3"
+                name="cityId"
+                onChange={onInputChange}
+              >
+                <option value="0">Choose city here</option>
+                <option value="1">first</option>
+                <option value="2">second</option>
+                <option value="3">third</option>
+              </Form.Select>
+            </Form.Group>
+          )}
+          <Form.Group className="form-group mb-3" controlId="information">
+            <Form.Label>Bio</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="bio"
+              rows={5}
+              defaultValue={user.bio}
+              placeholder="Enter your bio"
+              onChange={onInputChange}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="form-group mb-3" controlId="profilePicture">
+            <Form.Label>Upload your photo</Form.Label>
+            <Form.Control type="file" />
+          </Form.Group>
+          {error && <span className="text-danger">{error}</span>}
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+          <Form.Group>
+            <p>
+              Already have account ?
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </p>
+          </Form.Group>
+        </Form>
+      </div>
     </div>
   );
 }
