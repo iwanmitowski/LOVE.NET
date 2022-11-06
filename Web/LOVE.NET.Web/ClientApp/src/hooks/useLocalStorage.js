81c447997cd49444252d6a61df0347c4699f6e45
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-11-06 15:40:41 +0200

    Fixing invalid data in localstorage after logout

--- Web/LOVE.NET.Web/ClientApp/src/hooks/useLocalStorage.js
+++ Web/LOVE.NET.Web/ClientApp/src/hooks/useLocalStorage.js
@@ -4,13 +4,18 @@ export const useLocalStorage = (key, defaultValue) => {
   const [value, setValue] = useState(() => {
     const storedValue = localStorage.getItem(key);
 
-		return !!storedValue ? JSON.parse(storedValue) : defaultValue;
+    return !!storedValue ? JSON.parse(storedValue) : defaultValue;
   });
 
   const setLocalStorageValue = (newValue) => {
     localStorage.setItem(key, JSON.stringify(newValue));
 
-    setValue(newValue);
+    if(!!newValue) {
+      setValue(newValue);
+    }
+    else {
+      localStorage.removeItem(key);
+    }
   };
 
   return [value, setLocalStorageValue];
