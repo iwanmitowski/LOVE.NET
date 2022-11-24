de664d71773c06d82f79ffa83ca1d3c851556e8f
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-11-24 22:22:03 +0200

    Function to calculate distance

---
+++ Web/LOVE.NET.Web/ClientApp/src/utils/distance.js
@@ -0,0 +1,21 @@
+// https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+
+export function inKms(lat1, lon1, lat2, lon2) {
+  let R = 6371; // km
+  let dLat = toRad(lat2 - lat1);
+  let dLon = toRad(lon2 - lon1);
+  lat1 = toRad(lat1);
+  lat2 = toRad(lat2);
+
+  let a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let d = R * c;
+  return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) {
+  return (Value * Math.PI) / 180;
+}
