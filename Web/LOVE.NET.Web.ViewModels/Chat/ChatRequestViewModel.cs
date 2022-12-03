ce20462bf8047c3313f864ebe94ace6d03755d7c
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-12-03 13:20:25 +0200

    Validating user is in group

--- Web/LOVE.NET.Web.ViewModels/Chat/ChatRequestViewModel.cs
+++ Web/LOVE.NET.Web.ViewModels/Chat/ChatRequestViewModel.cs
@@ -1,9 +1,13 @@
 ﻿namespace LOVE.NET.Web.ViewModels.Chat
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class ChatRequestViewModel
     {
+        [Required]
         public string RoomId { get; set; }
 
+        [Required]
         public int Page { get; set; }
     }
 }
--- Web/LOVE.NET.Web/Controllers/ChatController.cs
+++ Web/LOVE.NET.Web/Controllers/ChatController.cs
@@ -1,8 +1,11 @@
 ﻿namespace LOVE.NET.Web.Controllers
 {
+    using System.Security.Claims;
+
     using LOVE.NET.Services.Chats;
     using LOVE.NET.Web.ViewModels.Chat;
 
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
 
@@ -18,10 +21,20 @@
             this.chatService = chatService;
         }
 
+        [Authorize]
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ChatViewModel))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public IActionResult GetChatMessages([FromBody] ChatRequestViewModel request)
         {
+            var loggedUserId = this.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            if (request?.RoomId.Contains(loggedUserId) == false)
+            {
+                return this.Forbid();
+            }
+
             // check user id is contained in the roomid
             var chat = this.chatService.GetChat(request);
 
