de02e1d2d2b3a1c3aebda4003665e38e83595bc3
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-12-11 09:36:13 +0200

    Gender service tests

---
+++ LOVE.NET.Services.Tests/GendersServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using LOVE.NET.Data.Common.Repositories;
+using LOVE.NET.Data.Models;
+using LOVE.NET.Data.Repositories.Users;
+using LOVE.NET.Services.Dashboard;
+using LOVE.NET.Services.Genders;
+
+namespace LOVE.NET.Services.Tests
+{
+    public class GendersServiceTests : TestsSetUp
+    {
+        private IGendersService gendersService;
+        private IRepository<Gender> gendersRepository;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            await GlobalInitialization();
+
+            gendersRepository = GetIRepository<Gender>();
+
+            gendersService = new GendersService(gendersRepository);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            dbContext.Database.EnsureDeleted();
+        }
+
+        [Test]
+        public void SuccessGetAll()
+        {
+            var genders = dbContext.Genders;
+
+            var result = gendersService.GetAll();
+
+            Assert.That(genders.Count(), Is.EqualTo(result.Count()));
+
+            foreach (var gender in genders)
+            {
+                Assert.True(result.Select(x => x.Id).Contains(gender.Id));
+            }
+        }
+    }
+}
