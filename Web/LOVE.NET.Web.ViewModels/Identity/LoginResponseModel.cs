1fa3d857bf7f637f225501d77715530d1da14961
DESKTOP-C1C2M07\Ivan <iwan.mitowski@gmail.com>
2022-12-13 21:31:43 +0200

    Mapping the user pfp

--- Web/LOVE.NET.Web.ViewModels/Identity/LoginResponseModel.cs
+++ Web/LOVE.NET.Web.ViewModels/Identity/LoginResponseModel.cs
@@ -1,12 +1,14 @@
 ﻿namespace LOVE.NET.Web.ViewModels.Identity
 {
     using System;
+    using System.Linq;
     using System.Text.Json.Serialization;
 
     using AutoMapper;
 
     using LOVE.NET.Data.Models;
     using LOVE.NET.Services.Mapping;
+
     public class LoginResponseModel : IMapFrom<ApplicationUser>, IHaveCustomMappings
     {
         public string Id { get; set; }
@@ -31,14 +33,18 @@
 
         public double Longitude { get; set; }
 
+        public string ProfilePicture { get; set; }
+
         [JsonIgnore]
         public string RefreshToken { get; set; }
+
 
         public void CreateMappings(IProfileExpression configuration)
         {
             configuration.CreateMap<ApplicationUser, LoginResponseModel>()
                 .ForMember(m => m.Latitude, opt => opt.MapFrom(x => x.City.Latitude))
-                .ForMember(m => m.Longitude, opt => opt.MapFrom(x => x.City.Longitude));
+                .ForMember(m => m.Longitude, opt => opt.MapFrom(x => x.City.Longitude))
+                .ForMember(m => m.ProfilePicture, opt => opt.MapFrom(x => x.Images.FirstOrDefault(i => i.IsProfilePicture).Url));
         }
     }
 }
