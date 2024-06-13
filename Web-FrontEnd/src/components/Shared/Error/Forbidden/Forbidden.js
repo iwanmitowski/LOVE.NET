12cdd7e064925d67fc41e5bf615c2d4e785bfb90
Ivan Mitovski <imitovski@nemetschek.bg>
2024-06-13 21:15:21 +0300

    Fix error pages

--- Web-FrontEnd/src/components/Shared/Error/Forbidden/Forbidden.js
+++ Web-FrontEnd/src/components/Shared/Error/Forbidden/Forbidden.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import styles from "../../../Shared/Forms.module.css";
 
-export default function NotFound() {
+export default function Forbidden() {
+  const formWrapperStyles = `${styles["form-wrapper"]} d-flex justify-content-center align-items-center`;
   return (
-    <div className="text-center">
-      <h1>Forbidden! You can't do this</h1>
-      <div className="mt-3">
-        <Link className="nav-link" to="/LOVE.NET">
-          Take me back
-        </Link>
+    <div className={formWrapperStyles}>
+      <div className="bg-light rounded shadow p-3">
+        <div className={styles["input-fields-length"]}>
+          <h3>Forbidden! You can't do this</h3>
+          <Link className="nav-link" to="/LOVE.NET">
+            Take me back
+          </Link>
+        </div>
       </div>
     </div>
   );
--- Web-FrontEnd/src/components/Shared/Error/NotFound/NotFound.js
+++ Web-FrontEnd/src/components/Shared/Error/NotFound/NotFound.js
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
+import styles from "../../../Shared/Forms.module.css";
 
 export default function NotFound() {
+  const formWrapperStyles = `${styles["form-wrapper"]} d-flex justify-content-center align-items-center`;
   return (
-    <div className="text-center">
-      <h1>Page not found</h1>
-      <div className="mt-3">
-        <Link className="nav-link" to="/LOVE.NET">
-          Take me back
-        </Link>
+    <div className={formWrapperStyles}>
+      <div className="bg-light rounded shadow p-3">
+        <div className={styles["input-fields-length"]}>
+          <h1>404</h1>
+          <h3>Page not found!</h3>
+          <Link className="nav-link" to="/LOVE.NET">
+            Take me back
+          </Link>
+        </div>
       </div>
     </div>
   );
